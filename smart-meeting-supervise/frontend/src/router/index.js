e1c03ff25978bca92d0e1c68ad6281906379d0d3
unknown <2542343808@qq.com>
2026-06-01 09:41:47 +0800

    refactor(supervise): 删除子系统独立登录页，统一使用平台登录

--- smart-meeting-supervise/frontend/src/router/index.js
+++ smart-meeting-supervise/frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
+
 import Layout from '../components/Layout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import ProblemList from '../views/ProblemList.vue'
